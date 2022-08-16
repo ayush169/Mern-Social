fb3132a5917740eb32f91fa8890aad2271ae9936
ayush169 <53981848+ayush169@users.noreply.github.com>
2022-08-16 22:15:22 +0530

    rep2 with changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -22,6 +23,7 @@ mongoose
 // Middleware
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
 app.use(morgan("common"));
 
 app.get("/", (req, res) => {
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,14 +4,23 @@ const bcrypt = require("bcrypt");
 
 // REGISTER
 router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, city, from, desc, relationship } =
+    req.body;
   try {
     // generate new password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // create new user
-    const newUser = new User({ username, email, password: hashedPassword });
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      city,
+      from,
+      desc,
+      relationship,
+    });
 
     // save user
     const user = await newUser.save();
